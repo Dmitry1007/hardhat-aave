365cb44bfe8e61f39e2e0bb2a8840d0250c2047f
Dmitry Sky <dmitry.sky@icloud.com>
2023-04-03 17:28:38 -0600

    Prettier stuffs

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,15 @@
-require("@nomiclabs/hardhat-waffle");
-require("@nomiclabs/hardhat-etherscan");
-require("hardhat-deploy");
-require("solidity-coverage");
-require("hardhat-gas-reporter");
-require("hardhat-contract-sizer");
-require("dotenv").config();
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
 
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
-const ETHERSCAN_API_KEY =
-    process.env.ETHERSCAN_API_KEY || "Your etherscan API key";
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "Your etherscan API key"
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -99,4 +98,4 @@ module.exports = {
     mocha: {
         timeout: 500000, // 500 seconds
     },
-};
+}
