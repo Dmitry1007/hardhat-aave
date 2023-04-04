a8c53c25a7a47f199413c144b3149ecf33d01e4c
Dmitry Sky <dmitry.sky@icloud.com>
2023-04-04 17:07:01 -0600

    Give approval to Aave lending pool to spend wETH and then deposit into lendingPool

--- scripts/aaveBorrow.js
+++ scripts/aaveBorrow.js
@@ -1,23 +1,44 @@
 const { getWeth } = require("./getWeth")
-const { getNamedAccounts } = require("hardhat")
+const { getNamedAccounts, ethers } = require("hardhat")
 const { networkConfig } = require("../helper-hardhat-config")
 
 async function main() {
     await getWeth()
-    const lendingPool = await getLendingPool()
+    const { deployer } = await getNamedAccounts()
+    const lendingPool = await getLendingPool(deployer)
     console.log(`LendingPool Address: ${lendingPool.address}`)
+
+    // deposit
+    const wethTokenAddress = networkConfig[network.config.chainId].wethToken
+    // approve
+    await approveErc20(
+        wethTokenAddress,
+        lendingPool.address,
+        ethers.utils.parseEther("0.1"),
+        deployer
+    )
+    console.log("Depositing...")
+    await lendingPool.deposit(wethTokenAddress, ethers.utils.parseEther("0.1"), deployer, 0)
+    console.log("Deposited!")
 }
 
-async function getLendingPool() {
-    const { deployer } = await getNamedAccounts()
+async function getLendingPool(account) {
     const lendingPoolAddressesProvider = await ethers.getContractAt(
         "ILendingPoolAddressesProvider",
         networkConfig[network.config.chainId].lendingPoolAddressesProvider,
-        deployer
+        account
     )
     const lendingPoolAddress = await lendingPoolAddressesProvider.getLendingPool()
-    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, deployer)
+    const lendingPool = await ethers.getContractAt("ILendingPool", lendingPoolAddress, account)
     return lendingPool
+}
+
+async function approveErc20(erc20Address, spenderAddress, amountToSpend, account) {
+    const erc20Token = await ethers.getContractAt("IERC20", erc20Address, account)
+    const txResponse = await erc20Token.approve(spenderAddress, amountToSpend)
+    await txResponse.wait(1)
+    const allowance = await erc20Token.allowance(account, spenderAddress)
+    console.log(`Allowance: ${allowance.toString()}`)
 }
 
 main()
